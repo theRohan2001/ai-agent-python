dc27c8e714cf8d220280c92aa359bec0c6932bf7
theRohan2001 <rohanwadhwa2001@gmail.com>
2025-12-25 20:04:23 +0000

    feat: implement actual function calling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from prompts import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,19 +23,21 @@ response = client.models.generate_content(
     contents=messages,
     config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]))
 
+function_results = []
+
 if response.function_calls:
     for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call, args.verbose)
+        if not function_call_result.parts:
+            raise Exception("call_function returned Content with no parts")
+        if function_call_result.parts[0].function_response is None:
+            raise Exception("Content.parts[0].function_response is None")
+        if function_call_result.parts[0].function_response.response is None:
+            raise Exception("FunctionResponse.response is None")
+        
+        function_results.append(function_call_result.parts[0])
+
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 else:
     print(f"Response: \n{response.text}")
-
-if response.usage_metadata:
-    prompt_token = response.usage_metadata.prompt_token_count
-    response_token = response.usage_metadata.candidates_token_count
-else:
-    raise RuntimeError("No response from the model")
-
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {prompt_token}")
-    print(f"Response tokens: {response_token}")
