ae46a679d6f22f3487ee1e7d12f319d46bd145fc
theRohan2001 <rohanwadhwa2001@gmail.com>
2025-12-26 06:32:07 +0000

    test: add test cases for function tools used by the LLM

---
+++ tests/test_get_files_info.py
@@ -0,0 +1,17 @@
+from functions.get_files_info import get_files_info
+
+def main():
+    print("Result for current directory:")
+    print(get_files_info("calculator", "."))
+
+    print("Result for 'pkg' directory:")
+    print(get_files_info("calculator", "pkg"))
+
+    print("Result for '/bin' directory:")
+    print(get_files_info("calculator", "/bin"))
+
+    print("Result for '../' directory:")
+    print(get_files_info("calculator", "../"))
+
+if __name__ == "__main__":
+    main()
